5f20b177a0203b29edafc4115aef2ec1476e9231
PatrickHickey <codewithpat@gmail.com>
2019-02-07 02:35:26 -0800

    Add book event

--- app.js
+++ app.js
@@ -50,5 +50,11 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', e => {
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+});
 
 // Event: Remove a Book
