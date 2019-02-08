c924fcd924fd8aa3a4614ea06b00154a3c2bcc2f
PatrickHickey <codewithpat@gmail.com>
2019-02-08 00:07:51 -0800

    Add book feature

--- app.js
+++ app.js
@@ -51,10 +51,18 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', e => {
+  // Prevent actual submit
+  e.preventDefault();
+
   // Get form values
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
+
+  // Instatiate book
+  const book = new Book(title, author, isbn);
+
+  console.log(book);
 });
 
 // Event: Remove a Book
